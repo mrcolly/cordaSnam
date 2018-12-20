07c6b2a8180ffe063c21854bff98ebebb00f544f
mrcolly <collytv@gmail.com>
2018-12-20 14:27:07 +0100

    modificato controllo di flussi e cambiato nomi ai nodi

---
+++ kotlin-source/start_nodes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd build/nodes
+cd Notary
+nohup java -jar corda.jar &
+sleep 2
+cd ..
+
+
+cd Sman
+nohup java -jar corda.jar &
+sleep 2
+nohup java -jar corda-webserver.jar &
+sleep 2
+cd ..
+
+
+cd EMI
+nohup java -jar corda.jar &
+sleep 2
+nohup java -jar corda-webserver.jar &
+sleep 2
+cd ..
+
+
+cd Songenia
+nohup java -jar corda.jar &
+sleep 2
+nohup java -jar corda-webserver.jar &
+sleep 2
+cd ..
+
+
+cd Edifon
+nohup java -jar corda.jar &
+sleep 2
+nohup java -jar corda-webserver.jar &
+sleep 2
+cd ..
