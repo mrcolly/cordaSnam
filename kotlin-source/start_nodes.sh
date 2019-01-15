e8eceb0080c83b6cfe46a4fff1a271af3abafd37
mrcolly <collytv@gmail.com>
2019-01-15 18:22:07 +0100

    update start_nodes.sh

--- kotlin-source/start_nodes.sh
+++ kotlin-source/start_nodes.sh
@@ -2,38 +2,37 @@
 
 cd /root/cordaSnam/kotlin-source/build/nodes
 cd Notary
-nohup java -jar corda.jar &
-sleep 2
+java -jar corda.jar &
+sleep 60
 cd ..
 
 
 cd Sman
-nohup java -jar corda.jar &
+java -jar corda.jar &
 sleep 2
-nohup java -jar corda-webserver.jar &
-sleep 2
+java -jar corda-webserver.jar &
+sleep 60
 cd ..
 
 
 cd EMI
-nohup java -jar corda.jar &
+java -jar corda.jar &
 sleep 2
-nohup java -jar corda-webserver.jar &
-sleep 2
+java -jar corda-webserver.jar &
+sleep 60
 cd ..
 
 
 cd Songenia
-nohup java -jar corda.jar &
+java -jar corda.jar &
 sleep 2
-nohup java -jar corda-webserver.jar &
-sleep 2
+java -jar corda-webserver.jar &
+sleep 60
 cd ..
 
 
 cd Edifon
-nohup java -jar corda.jar &
+java -jar corda.jar &
 sleep 2
-nohup java -jar corda-webserver.jar &
-sleep 2
-cd ..
+java -jar corda-webserver.jar &
+sleep infinity
